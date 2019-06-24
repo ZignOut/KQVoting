05b1607c9fbce064ec06289676be153268f590cf
ZignOut <kaunghtut703@gmail.com>
2019-06-24 20:26:26 +0630

    auth commit

--- routes/api.php
+++ routes/api.php
@@ -17,20 +17,23 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('student', 'studentController@index');
-Route::get('student/{stuid}', 'studentController@show');
-Route::post('student', 'studentController@store');
-Route::put('student/{stuid}', 'studentController@update');
-Route::delete('student/{stuid}', 'studentController@delete');
+Route::group(['middleware'=>'cors'], function(){
 
-Route::get('selection', 'selectionController@index');
-Route::get('selection/{selid}', 'selectionController@show');
-Route::post('selection', 'selectionController@store');
-Route::put('selection/{selid}', 'selectionController@update');
-Route::delete('selection/{selid}', 'selectionController@delete');
+    Route::get('student', 'studentController@index');
+    Route::get('student/{stuid}', 'studentController@show');
+    Route::post('student', 'studentController@store');
+    Route::put('student/{stuid}', 'studentController@update');
+    Route::delete('student/{stuid}', 'studentController@delete');
 
-Route::get('vote', 'voteController@index');
-Route::get('vote/{stuid}', 'voteController@show');
-Route::post('vote', 'voteController@store');
-Route::put('vote/{stuid}', 'voteController@update');
-Route::delete('vote/{stuid}', 'voteController@delete');
+    Route::get('selection', 'selectionController@index');
+    Route::get('selection/{selid}', 'selectionController@show');
+    Route::post('selection', 'selectionController@store');
+    Route::put('selection/{selid}', 'selectionController@update');
+    Route::delete('selection/{selid}', 'selectionController@delete');
+
+    Route::get('vote', 'voteController@index');
+    Route::get('vote/{stuid}', 'voteController@show');
+    Route::post('vote', 'voteController@store');
+    Route::put('vote/{stuid}', 'voteController@update');
+    Route::delete('vote/{stuid}', 'voteController@delete');
+});
